a5437123d22192be4d87698f7335b18bf38c659b
ashley <115029737+sugaricedreams@users.noreply.github.com>
2025-03-10 14:18:36 -0500

    finished chart

--- showcase/examples/turbidity-chart/turbidity-chart.js
+++ showcase/examples/turbidity-chart/turbidity-chart.js
@@ -1,57 +1,60 @@
 // create data source for Android phone camera
 import ChartJsView from 'osh-js/core/ui/view/chart/ChartJsView.js';
 import CurveLayer from 'osh-js/core/ui/layer/CurveLayer.js';
-import SweApi from 'osh-js/core/datasource/sweapi/SweApi.datasource.js';
 import SosGetResultJson from 'osh-js/core/datasource/SosGetResultJson.js';
 
-let chartDataSource = new SweApi("Turbidity", {
+    let chartDataSource = new SosGetResultJson("Turbidity", {
         protocol: "ws",
-        endpointUrl: "localhost:8181/sensorhub/api",
-        resource: "/datastreams/$rbh5gva7tgl96/observations",
-        startTime: "T20:35:55Z",
-        endTime: "T20:46:03Z",
-        mode: Mode.REPLAY,
+        service: "SOS",
+        endpointUrl: "localhost:8181/sensorhub/sos",
+        offeringID: "[URN]turbidity001",
+        observedProperty: "urn:osh:datastream:TDSOutput:all_properties",
+        startTime: "2025-02-26T20:35:55Z",
+        endTime: "2025-02-26T20:46:03Z",
     });
 
-//    let chartDataSource = new SosGetResultJson("weather", {
-//        protocol: "ws",
-//        service: "SOS",
-//        endpointUrl: "sensiasoft.net:8181/sensorhub/sos",
-//        offeringID: "urn:mysos:offering03",
-//        observedProperty: "http://sensorml.com/ont/swe/property/Weather",
-//        startTime: "now",
-//        endTime: "2055-01-01Z"
-//    });
+    console.log("DataSource ID:", chartDataSource.id);
 
 // #region snippet_curve_layer
 let MVLayerCurve = new CurveLayer({
-    //console.log(rec);
     dataSourceId: chartDataSource.id,
-    getValues: (rec, timeStamp) => {
-        return {
-            x: new Date(timeStamp).getTime(),
-            y: parseFloat(rec.milliVolts.replace(/[^\d.-]/g, ''))
-        }
+    getValues: (rec, Timestamp) => {
+    console.log("Processing record:", rec);
+            if (rec && rec.Millivolts && rec.Timestamp) {
+                console.log("Timestamp:", rec.Timestamp);
+                console.log("MilliVolts:", parseFloat(rec.Millivolts.replace(/[^\d.-]/g, '')));
+                return {
+                      x: rec.Timestamp,
+                      y: parseFloat(rec.Millivolts.replace(/[^\d.-]/g, ''))
+                };
+            } else {
+                console.log("Record does not contain milliVolts.");
+            }
+            return null;
     },
     name: 'Millivolts (mV)'
+
 });
 
 let PPMLayerCurve = new CurveLayer({
-    //console.log(rec);
     dataSourceId: chartDataSource.id,
-    getValues: (rec, timeStamp) => {
-        return {
-            x: new Date(timeStamp).getTime(),
-            y: parseFloat(rec.partsPerMil.replace(/[^\d.-]/g, ''))
+    getValues: (rec, Timestamp) => {
+    console.log("Processing next record:", rec);
+        if (rec && rec["Parts Per Million"] && rec.Timestamp) {
+            console.log("Timestamp:", rec.Timestamp);
+            console.log("Parts Per Million:", parseFloat(rec["Parts Per Million"].replace(/[^\d.-]/g, '')));
+            return {
+                  x: rec.Timestamp,
+                  y: parseFloat(rec["Parts Per Million"].replace(/[^\d.-]/g, ''))
+            };
+        } else {
+            console.log("Record does not contain Parts Per Million.");
         }
+        return null;
     },
-    name: 'Parts Per Million (ppm)'
+    name: 'Parts Per Million (PPM)'
 });
-// #endregion snippet_curve_layer
-
-//chartDataSource.onDataReceived = (data) => {
-//    console.log("Data received:", data);
-//};
+ //#endregion snippet_curve_layer
 
 // show it in video view
 let chartView = new ChartJsView({
@@ -63,10 +66,11 @@ let chartView = new ChartJsView({
             scales: {
                 yAxes: [{
                     scaleLabel: {
+                        labelString: "Parts Per Million (PPM)",
                         labelString: "Millivolts (mV)"
                     },
                     ticks: {
-                        maxTicksLimit: 10
+                        maxTicksLimit: 20
                     }
                 }],
                 xAxes: [{
@@ -79,11 +83,21 @@ let chartView = new ChartJsView({
                 }],
             }
         },
-        datasetsProps: {
-            backgroundColor: 'rgba(141,242,246, 0.1)'
-        }
+//        datasetsProps: { // these dont work for whatever reason
+//            //backgroundColor: 'rgba(141,242,246, 0.1)'
+////            borderColor: 'rgba(255, 183, 51, 0.8)', // Line color
+////            backgroundColor: 'rgba(255, 217, 148, 0.8)', // Fill color under the line
+////            pointBackgroundColor: '#fff7ab', // Point color
+////            pointBorderColor: 'rgba(255, 183, 51, 0.8)', // Border color around points
+////            pointHoverBackgroundColor: 'rgba(255, 251, 50, 0.8)', // Point hover background color
+////            pointHoverBorderColor: 'rgba(255, 88, 50, 0.8)' // Point hover border color
+//        }
     }
 });
 
+
+console.log("Attempting to connect to data source");
 // start streaming
 chartDataSource.connect();
+console.log("connected to data source");
+
